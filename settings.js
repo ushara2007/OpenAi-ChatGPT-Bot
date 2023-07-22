9544c0c441d9ed7f12509aab4483a90577e30da3
ushara2007 <139131061+ushara2007@users.noreply.github.com>
2023-07-22 16:40:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = 'king-nima-gpt-bot'
-global.SESSION_ID = ''
+global.SESSION_ID = 'elisa_md@;;;IF9V2DAD#CVUIueJNjbKlRdaH1RMtNlu9pnVfIFMAlh8EluRWRV8'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
